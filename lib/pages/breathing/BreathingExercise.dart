aa9a19361a4f49f1de582e4657da7e712f0b8ec3
unknown <nacuandrei10@gmail.com>
2022-06-11 17:11:35 +0300

    Initial commit + assets/audio folder + pubspec.yaml configurations

--- lib/pages/breathing/BreathingExercise.dart
+++ lib/pages/breathing/BreathingExercise.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 class BreathingExercise extends StatelessWidget {
@@ -5,6 +6,7 @@ class BreathingExercise extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(title: Text("")),
       body: Center(
