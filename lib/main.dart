2f7dce7eec77d8e1fda488d9d853620ef16ed2dc
petrupopescu1 <pscu.ptr@gmail.com>
2022-06-12 14:51:51 +0300

    added daily reminder to fill in calendar notif

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,34 @@ import 'package:mood_tracker/pages/quotes/Quotes.dart';
 import 'package:mood_tracker/pages/settings/Settings.dart';
 import 'package:mood_tracker/pages/statistics/Statistics.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:cron/cron.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  AwesomeNotifications().initialize(
+      null,
+      [
+        NotificationChannel(
+            channelKey: 'key1',
+            channelName: 'Proto Coders Point',
+            channelDescription: "Notification example",
+            defaultColor: const Color(0XFF9050DD),
+            ledColor: Colors.white,
+            playSound: true,
+            enableLights:true,
+            enableVibration: true
+        )
+      ]
+  );
   await Hive.initFlutter();
   Hive.registerAdapter(RecordAdapter());
   await Hive.openBox('journal');
+  final cron = Cron();
+  cron.schedule(Schedule.parse('* 18 * * *'), () async {
+    print('Runs every Five seconds');
+    notify();
+  });
   runApp(const MoodTracker());
 }
 
@@ -43,4 +66,15 @@ class MoodTracker extends StatelessWidget {
           );
         });
   }
+}
+
+void notify() async{
+  await AwesomeNotifications().createNotification(
+      content: NotificationContent(
+          id: 1,
+          channelKey: 'key1',
+          title:'Reminder',
+          body: 'Don\'t forget to fill in the calendar'
+      )
+  );
 }
