ac33cb5d6e410e43d0da39105d3bca91575a6471
unknown <nacuandrei10@gmail.com>
2022-06-13 11:18:09 +0300

    Added class and state to TodoList

--- lib/pages/objectives/Objectives.dart
+++ lib/pages/objectives/Objectives.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'TodoList.dart';
+
 class Objectives extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
---
+++ lib/pages/objectives/TodoList.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class TodoList extends StatefulWidget {
+  @override
+  _TodoListState createState() => new _TodoListState();
+}
