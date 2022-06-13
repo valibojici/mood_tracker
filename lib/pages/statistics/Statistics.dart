6f95d6a804106c143b5d7f21d2aa074920c5d954
petrupopescu1 <pscu.ptr@gmail.com>
2022-06-13 16:46:40 +0300

    added initial chart for last 7 days

--- lib/pages/statistics/Statistics.dart
+++ lib/pages/statistics/Statistics.dart
@@ -1,5 +1,10 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:pie_chart/pie_chart.dart';
+
+import '../../model/Record.dart';
 /*
   Aici ar fi ceva statistici, poate un grafic ceva
   O sa trebuiasca sa luam din baza de date cum s a simtit utilizatorul
@@ -13,14 +18,55 @@ class Statistics extends StatefulWidget {
 }
 
 class _StatisticsState extends State<Statistics> {
+  Map <String, double> moodMap =
+  {
+    "Very sad" : 0,
+    "Sad": 0,
+    "Neutral": 0,
+    "Happy" : 0,
+    "Very happy": 0
+  };
+  static final List<Color> moodColors = [
+    Colors.deepPurple,
+    Colors.purple,
+    Colors.blue,
+    Colors.teal,
+    Colors.green
+  ];
   @override
   Widget build(BuildContext context) {
+    Box records = Hive.box("journal");
+    List<Record> L = records.values.cast<Record>().toList();
+    int k = 7;
+    int mn = max(L.length - k - 1, 0);
+    for(int i = L.length - 1; i >= mn; i--)
+      {
+        switch(L[i].mood)
+        {
+          case 0:
+            moodMap["Very sad"] = (moodMap["Very sad"]! + 1);
+            break;
+          case 1:
+            moodMap["Sad"] = (moodMap["Sad"]! + 1);
+            break;
+          case 2:
+            moodMap["Neutral"] = (moodMap["Neutral"]! + 1);
+            break;
+          case 3:
+            moodMap["Happy"] = (moodMap["Happy"]! + 1);
+            break;
+          case 4:
+            moodMap["Very happy"] = (moodMap["Very happy"]! + 1);
+            break;
+        }
+      }
     return Scaffold(
       appBar: AppBar(title: const Text("Statistics")),
-      body: const Center(
-        child: Text(
-            "Aici ar trebui sa fie ceva statistici, un pie chart/grafic ceva?"),
+      body: Center(
+        child: PieChart(
+          dataMap: moodMap,
+          colorList: moodColors,
       ),
-    );
+    ));
   }
 }
